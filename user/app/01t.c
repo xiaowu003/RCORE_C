affeba8e23851f7b5777b9958c5a72de1ab3cf6d
xiaowu <2229048563@qq.com>
2025-01-07 14:02:36 +0000

    ch3-coop base ch2

--- user/app/01t.c
+++ user/app/01t.c
@@ -3,5 +3,10 @@
 
 int64 main(void) {
     printf("[APP_01] app switch success\n");
+    printf("[APP_01] try to execute privileged\n");
+    asm volatile ("sret"
+                   : 
+                   : 
+                   : );
     return 0;
 }
